996f3ce1a33cb0898ab7855fcf074ae6b5a66ae8
Nikita Semichev <nsemichev@gmail.com>
2020-06-22 22:50:05 -0400

    add Linear stacks

--- src/BeatmapGenerator.java
+++ src/BeatmapGenerator.java
@@ -15,11 +15,16 @@ public class BeatmapGenerator {
     private float startTimePoint;
     private float endTimePoint;
 
+    private int linearStackSpacing = 25;
+
     public String generateBeatmap(String type){
         if(type.contains("RS")){
             return randomStack(Integer.parseInt(type.substring(2)));
         }
-
+        if(type.contains("LS")){
+            String[] params = type.split(",");
+            return linearStack(Integer.parseInt(params[1]), Integer.parseInt(params[2]));
+        }
         else {
             System.out.println("Unrecognized type.");
             System.exit(0);
@@ -30,18 +35,115 @@ public class BeatmapGenerator {
     private String randomStack(int num){
         float curTime = startTimePoint;
         String objects = "[HitObjects]\n";
+        int curCombo = 0;
         while(curTime < endTimePoint){
             //if(curTime + halfTick + (num - 1) * quarterTick > endTimePoint) break;
             float[] timings = generateTimingPattern(curTime, num);
             Random random = new Random();
-            int x_coord = random.nextInt(max_x+1);
-            int y_coord = random.nextInt(max_y+1);
+            int x_coord = random.nextInt(max_x-20)+10;
+            int y_coord = random.nextInt(max_y-20)+10;
             for(int i = 0; i < num; i++){
-                objects += String.valueOf(x_coord) + "," + String.valueOf(y_coord) + "," + String.valueOf((int) timings[i]) + ",1,0" + "\n";
+                if(i == 0 && curCombo + num > 16){
+                    objects += String.valueOf(x_coord) + "," + String.valueOf(y_coord) + "," + String.valueOf((int) timings[i]) + ",5,0" + "\n";
+                    curCombo = 0;
+                }
+                else objects += String.valueOf(x_coord) + "," + String.valueOf(y_coord) + "," + String.valueOf((int) timings[i]) + ",1,0" + "\n";
             }
             curTime += halfTick + (num - 1) * quarterTick;
+            curCombo += num;
         }
         return objects;
+    }
+
+    private String linearStack(int num, int spacing){
+        Random random = new Random();
+        int start_x = random.nextInt(max_x-20)+10;
+        int start_y = random.nextInt(max_y-20)+10;
+        float curTime = startTimePoint;
+        String objects = "[HitObjects]\n";
+        int lastDir = 0; // -1 = N -2 = E 1 = S 2 = W
+        int cur_x = start_x;
+        int cur_y = start_y;
+        int curCombo = 0;
+        while(curTime < endTimePoint){
+            int randNum = random.nextInt(num) + 1;
+            if (randNum % 2 == 0) randNum += 1;
+            //if(curTime + halfTick + (num - 1) * quarterTick > endTimePoint) break;
+            float[] timings = generateTimingPattern(curTime, randNum);
+            int[] x_coords = new int[randNum];
+            int[] y_coords = new int[randNum];
+
+            int nextDir = chooseDir(lastDir, spacing, cur_x, cur_y, randNum);
+
+            int incrX = 0;
+            int incrY = 0;
+
+            if(nextDir == -2){
+                cur_x += spacing;
+                incrX = 1;
+            }
+            else if (nextDir == -1){
+                cur_y -= spacing;
+                incrY = -1;
+            }
+            else if (nextDir == 1){
+                cur_y += spacing;
+                incrY = 1;
+            }
+            else {
+                cur_x -= spacing;
+                incrX = -1;
+            }
+
+            x_coords[0] = cur_x;
+            y_coords[0] = cur_y;
+
+            for(int i = 1; i < randNum; i++){
+                x_coords[i] = x_coords[i-1] + incrX * linearStackSpacing;
+                y_coords[i] = y_coords[i-1] + incrY * linearStackSpacing;
+            }
+
+
+
+            for(int i = 0; i < randNum; i++){
+                if(i == 0 && curCombo + randNum > 16) {
+                    objects += String.valueOf(x_coords[i]) + "," + String.valueOf(y_coords[i]) + "," + String.valueOf((int) timings[i]) + ",5,0" + "\n";
+                    curCombo = 0;
+                }
+                else objects += String.valueOf(x_coords[i]) + "," + String.valueOf(y_coords[i]) + "," + String.valueOf((int) timings[i]) + ",1,0" + "\n";
+            }
+
+            curTime += halfTick + (randNum - 1) * quarterTick;
+            lastDir = nextDir;
+            cur_x = x_coords[randNum-1];
+            cur_y = y_coords[randNum-1];
+            curCombo += randNum;
+        }
+        return objects;
+    }
+
+    private int chooseDir(int lastDir, int spacing, int x_coord, int y_coord, int num){
+        if(lastDir == 0) {
+            while(true){
+                Random random = new Random();
+                int nextDir = random.nextInt(5) - 2;
+                if(nextDir == 1 && (y_coord + spacing + linearStackSpacing *  (num - 1) < 374)) return nextDir;
+                if(nextDir == -1 && (y_coord - spacing - linearStackSpacing *  (num - 1) > 10)) return nextDir;
+                if(nextDir == -2 && (x_coord + spacing + linearStackSpacing *  (num - 1) < 502)) return nextDir;
+                if(nextDir == 2 && (x_coord - spacing - linearStackSpacing *  (num - 1) > 10)) return nextDir;
+            }
+        }
+        else{
+            while(true){
+                Random random = new Random();
+                int nextDir = random.nextInt(5) - 2;
+                if(nextDir + lastDir == 0) continue;
+                if(nextDir == 1 && (y_coord + spacing + linearStackSpacing *  (num - 1) < 374)) return nextDir;
+                if(nextDir == -1 && (y_coord - spacing - linearStackSpacing *  (num - 1) > 10)) return nextDir;
+                if(nextDir == -2 && (x_coord + spacing + linearStackSpacing *  (num - 1) < 502)) return nextDir;
+                if(nextDir == 2 && (x_coord - spacing - linearStackSpacing *  (num - 1) > 10)) return nextDir;
+            }
+        }
     }
 
     private float[] generateTimingPattern(float curTime, int num){
--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class Driver {
-    //private static String DIR_PATH = "C:\\Users\\Nikita\\AppData\\Local\\osu!\\Songs\\751896 Cranky - Rave 2 Rave\\";
-    private static String DIR_PATH = "/Applications/osu!.app/Contents/Resources/drive_c/osu!/Songs/558694 xi - Glorious Crown/xi - Glorious Crown (Monstrata) [FOUR DIMENSIONS].osu";
+    private static String DIR_PATH = "C:\\Users\\Nikita\\AppData\\Local\\osu!\\Songs\\11742 Rossini & MC F 40 - Guillaume Tell\\Rossini & MC F 40 - Guillaume Tell (Sushi971) [Heavy].osu";
+    //private static String DIR_PATH = "/Applications/osu!.app/Contents/Resources/drive_c/osu!/Songs/558694 xi - Glorious Crown/xi - Glorious Crown (Monstrata) [FOUR DIMENSIONS].osu";
     public static void main(String[] args){
         Scanner sc  = new Scanner(System.in);
         System.out.print("Enter filename: ");
@@ -22,7 +22,8 @@ public class Driver {
         BeatmapGenerator bg = new BeatmapGenerator(song);
         song.setObjects(bg.generateBeatmap(config));
 
-        fileReader.writeFile(String.format("/Applications/osu!.app/Contents/Resources/drive_c/osu!/Songs/558694 xi - Glorious Crown/xi - Glorious Crown (Monstrata) [FOUR DIMENSIONS-%s].osu", config), song);
+        fileReader.writeFile(String.format("C:\\Users\\Nikita\\AppData\\Local\\osu!\\Songs\\11742 Rossini & MC F 40 - Guillaume Tell\\Rossini & MC F 40 - Guillaume Tell (Sushi971) [Heavy-%s].osu", config), song);
+        //fileReader.writeFile(String.format("/Applications/osu!.app/Contents/Resources/drive_c/osu!/Songs/558694 xi - Glorious Crown/xi - Glorious Crown (Monstrata) [FOUR DIMENSIONS-%s].osu", config), song);
 
     }
 }
